1fa45c051e8f38b430eccda2e872449d4532764a
KGGeorgiev21 <KGGeorgiev21@codingburgas.bg>
2022-06-22 22:46:34 +0300

    Add home title

--- Website/cache/index.html.6a40eb1d283c251f6cccdb44bd980b17.rtpl.php
+++ Website/cache/index.html.6a40eb1d283c251f6cccdb44bd980b17.rtpl.php
@@ -1,7 +1,25 @@
 <?php if(!class_exists('raintpl')){exit;}?><?php $tpl = new RainTPL;$tpl_dir_temp = self::$tpl_dir;$tpl->assign( $this->var );$tpl->display( dirname("header.html") . ( substr("header.html",-1,1) != "/" ? "/" : "" ) . basename("header.html") );?>
 
 <div class="home">
-  
+    <div class="section-one">
+      <div class="column">
+        <h1>Welcome, adventurers</h1>
+        <p>Here you can find more information about our game filled with adventures and riddles. Start your journey on a deserted island, find the pirate treasure and survive!</p>
+      </div>
+      <div class="column">
+        <img src="" alt="Image here...">
+      </div>
+    </div>
+    <div class="section-two">
+      <h1></h1>
+      <p></p>
+      <a href=""></a>
+    </div>
+    <div class="section-three">
+      <form>
+        
+      </form>
+    </div>
 </div>
 
 <?php $tpl = new RainTPL;$tpl_dir_temp = self::$tpl_dir;$tpl->assign( $this->var );$tpl->display( dirname("footer.html") . ( substr("footer.html",-1,1) != "/" ? "/" : "" ) . basename("footer.html") );?>
